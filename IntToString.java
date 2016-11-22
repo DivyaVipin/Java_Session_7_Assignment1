f7d46171915c2a933eed04316cd52f16214a6ced
DivyaVipin <divyababu1001@gmail.com>
2016-11-22 12:50:31 +0530

    Add files via upload

---
+++ IntToString.java
@@ -0,0 +1,13 @@
+
+public class IntToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=2;
+		System.out.println("Integer value="+i);
+		String convert=Integer.toString(i);
+		System.out.println("String value of Integer="+convert);
+	}
+
+}
